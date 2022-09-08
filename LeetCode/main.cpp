a2cb6bb614fc62754ce5aeaadd1f9b4a11860837
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-08 18:37:07 +0300

    LargestNumberAtLeastTwiceofOthers.cpp

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
 
@@ -83,13 +84,59 @@ public:
     }
 };
 
+namespace Largest_Number_At_Least_Twice_of_Others {
+
+class Solution {
+public:
+    int dominantIndex(vector<int>& nums) {
+        int first_max = *nums.begin();
+        int second_max = *(nums.begin() + 1);
+        size_t first_max_index = 0;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (nums[i] > first_max) {
+                second_max = first_max;
+                first_max = nums[i];
+                first_max_index = i;
+            } else if (nums[i] > second_max && nums[i] != first_max) {
+                second_max = nums[i];
+            }
+        }
+        return first_max >= second_max * 2 ? first_max_index : -1;
+
+    }
+};
+
+void Tests() {
+    Solution solution;
+    {
+        vector<int> nums = {3,6,1,0};
+        assert(solution.dominantIndex(nums) == 1);
+    }
+    {
+        vector<int> nums = {1,2,3,4};
+        assert(solution.dominantIndex(nums) == -1);
+    }
+    {
+        vector<int> nums = {1,4};
+        assert(solution.dominantIndex(nums) == 1);
+    }
+    {
+        vector<int> nums = {4,1};
+        assert(solution.dominantIndex(nums) == 0);
+    }
+    {
+        vector<int> nums = {4,4};
+        assert(solution.dominantIndex(nums) == -1);
+    }
+}
+
+}
+
 
 
 
 int main()
 {
-    Solution11 sol;
-    vector<int> height {1,2,1};
-    cout << sol.maxArea(height) << endl;
+    Largest_Number_At_Least_Twice_of_Others::Tests();
     return 0;
 }
