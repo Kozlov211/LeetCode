7d8963e9d39e6338c02cc833bbe41ba7fcbd9aa8
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-10 14:03:06 +0300

    ClimbingStairs.cpp

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -237,6 +237,37 @@ void Tests() {
 
 }
 
+namespace Climbing_Stairs {
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        vector<int> count (n + 1);
+        count[0] = 1;
+        count[1] = 1;
+        for (size_t i = 2; i < count.size(); ++i) {
+            count[i] = count[i - 1] +  count[i - 2];
+        }
+        return count[n];
+    }
+};
+
+void Tests() {
+    Solution solution;
+    assert(solution.climbStairs(1) == 1);
+    assert(solution.climbStairs(2) == 2);
+    assert(solution.climbStairs(3) == 3);
+    assert(solution.climbStairs(4) == 5);
+    assert(solution.climbStairs(5) == 8);
+
+}
+
+
+}
+
 
 namespace Jump_Game {
 
@@ -286,6 +317,6 @@ void Tests() {
 
 int main()
 {
-    Jump_Game::Tests();
+    Climbing_Stairs::Tests();
     return 0;
 }
