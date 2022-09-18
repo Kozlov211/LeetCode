35ce45bc3a8dc48f2b58de112864b8e5ce4f5566
kozlov211 <sayf1ck64.7@gmail.com>
2022-09-18 18:24:57 +0300

    Numberof1Bits.cpp - 191

--- LeetCode/main.cpp
+++ LeetCode/main.cpp
@@ -709,8 +709,34 @@ void Tests() {
 
 }
 
+namespace Number_of_1_Bits {
+
+class Solution {
+public:
+    int hammingWeight(uint32_t number) {
+        int sum = 0;
+        while(number > 0) {
+            if (number & 1 > 0) {
+                ++sum;
+            }
+            number = number >> 1;
+        }
+        return sum;
+    }
+};
+
+void Tests() {
+    Solution solution;
+    uint32_t n = 00000000000000000000000000001011;
+    assert(solution.hammingWeight(n) == 3);
+    n = 00000000000000000000000010000000;
+    assert(solution.hammingWeight(n) == 1);
+}
+
+}
+
 int main()
 {
-    Prime_Number_of_Set_Bits_in_Binary_Representation::Tests();
+    Number_of_1_Bits::Tests();
     return 0;
 }
